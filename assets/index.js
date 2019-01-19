70f641baa920c72d8017b17493aef957733eea89
BGorson <BGorson32@gmail.com>
2019-01-19 17:56:09 -0600

    updated AJAX

--- assets/index.js
+++ assets/index.js
@@ -1,4 +1,4 @@
-var URL= 'https://www.eventbriteapi.com/v3/events/search/?q=music&location.address=Chicago&token=TPQYCAU53IO2TT2FQOOY'
+var URL= 'https://www.eventbriteapi.com/v3/events/search/?q=music&location.address=Chicago&start_date.keyword=this_week&token=TPQYCAU53IO2TT2FQOOY'
 
 
 $.ajax({
@@ -14,9 +14,11 @@ $.ajax({
     // Methods run on jQuery selectors return the selector they we run on
     // This is why we can create and save a reference to a td in the same statement we update its text
     var Tab1 = $("<td>").text(response.events[i].name.text);
+    var Tab2 = $("<td>").text(response.events[i].description.text);
+    var Tab3 = $("<td>").text(response.events[i].start.local);
       
     // Append the newly created table data to the table row
-    tRow.append(Tab1);
+    tRow.append(Tab1,Tab2,Tab3);
     // Append the table row to the table body
     $("tbody").append(tRow);
     }
