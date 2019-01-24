f334c7e6df18d0f2962b9ee6f933d7acbc1fa335
Ryan Gartner <ryanjgartner@gmail.com>
2019-01-23 21:13:38 -0600

    Onclick event to redirect pages

---
+++ assets/javascript/index.js
@@ -0,0 +1,11 @@
+//Begin Javascript
+
+//Onclick event to redirect to Create Page
+document.getElementById("create").onclick = function() {
+    location.src = "../create.html";
+};
+
+//Onclick event to redirect to Join Page
+document.getElementById("join").onclick = function() {
+    location.src = "../join.html";
+}
