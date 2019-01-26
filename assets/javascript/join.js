d07fa54931ecbb809c088181e14a5596b4d30ad2
Gastonm1 <gaston.marques@gmail.com>
2019-01-26 12:43:24 -0600

    Update join.js

--- assets/javascript/join.js
+++ assets/javascript/join.js
@@ -1,29 +1,47 @@
-console.log("test")
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyAHx0C7Hk2TJHRI5iL9RMY6YuExixeUCTM",
+  authDomain: "project1-1a4da.firebaseapp.com",
+  databaseURL: "https://project1-1a4da.firebaseio.com",
+  projectId: "project1-1a4da",
+  storageBucket: "project1-1a4da.appspot.com",
+  messagingSenderId: "803507948503"
+};
+firebase.initializeApp(config);
+
+
+var database = firebase.database();
+var groupRef = database.ref("group/")
+
+
 //Retrieves favorite items if available
-var eventInfo = (localStorage.getItem("joinGroup"));
-eventInfo=(JSON.parse(eventInfo))
-if (eventInfo == undefined){
+var eventInfo = localStorage.getItem("joinGroup");
+eventInfo = JSON.parse(eventInfo);
+if (eventInfo == undefined) {
   eventInfo = [];
 }
 
-$(document).on("click", ".joinGroup",function(){
-  
-    var info = {
-    eventId: $(this).attr("eventId"),
-  }
+$(document).on("click", ".joinGroup", function() {
+  var info = {
+    eventId: $(this).attr("eventId")
+  };
   eventInfo.push(info);
-  localStorage.setItem("joinGroup",JSON.stringify(eventInfo));
-  })
+  localStorage.setItem("joinGroup", JSON.stringify(eventInfo));
+});
 
+$(".getLocal").on("click", function() {
+  var storedEvents = localStorage.getItem("joinGroup");
+  console.log(storedEvents);
+  storedEvents = JSON.parse(storedEvents);
+  console.log(storedEvents[0].eventId);
+  for (i = 0; i < storedEvents.length; i++) {
+    var div = $("<div class= 'btnText'>" + storedEvents[i].eventId + "<div>");
+    $(".buttons").append(div);
+  }
+});
 
-$(".getLocal").on("click",function(){
-    var storedEvents = (localStorage.getItem("joinGroup"));
-    console.log(storedEvents)
-    storedEvents=(JSON.parse(storedEvents))
-    console.log(storedEvents[0].eventId)
-    for (i=0;i<storedEvents.length;i++){
-      var div= $("<div class= 'btnText'>"+storedEvents[i].eventId+  "<div>");
-        $(".buttons").append(div)
-    }
-  
-  })
+$("#join").on("click", function() {
+  var searchLocalStorageName = localStorage.getItem("groupName");
+  console.log(searchLocalStorageName);
+
+});
