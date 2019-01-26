bb62727876c780e2c932bbb34d56356bf0db62d1
Gastonm1 <gaston.marques@gmail.com>
2019-01-26 11:31:16 -0600

    Update join.js

--- assets/javascript/join.js
+++ assets/javascript/join.js
@@ -1,29 +1,23 @@
-console.log("test")
+console.log("test");
 //Retrieves favorite items if available
-var eventInfo = (localStorage.getItem("groupName"));
-eventInfo=(JSON.parse(eventInfo))
-if (eventInfo == undefined){
+var eventInfo = localStorage.getItem("groupName");
+if (eventInfo == undefined) {
   eventInfo = [];
 }
 
-$(document).on("click", ".joinGroup",function(){
-  
-    var info = {
-    eventId: $(this).attr("eventId"),
-  }
-  eventInfo.push(info);
-  localStorage.setItem("groupName",JSON.stringify(eventInfo));
-  })
+// $(document).on("click", ".joinGroup",function(){
 
+//     var info = {
+//     eventId: $(this).attr("eventId"),
+//   }
+//   eventInfo.push(info);
+//   localStorage.setItem("groupName",JSON.stringify(eventInfo));
+//   })
 
-$(".getLocal").on("click",function(){
-    var storedEvents = (localStorage.getItem("groupName"));
-    console.log(storedEvents)
-    storedEvents=(JSON.parse(storedEvents))
-    console.log(storedEvents[0].groupName)
-    for (i=0;i<storedEvents.length;i++){
-      var div= $("<div class= 'btnText'>"+storedEvents[i].groupName+  "<div>");
-        $(".buttons").append(div)
-    }
-  
-  })
+$(".getLocal").on("click", function() {
+  var storedEvents = localStorage.getItem("groupName");
+  console.log(storedEvents);
+
+  var div = $("<div class= 'btnText'>" + storedEvents + "<div>");
+  $(".buttons").append(div);
+});
