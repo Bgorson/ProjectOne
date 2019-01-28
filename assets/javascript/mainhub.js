72944ee91c1ea642bdc7787220850b68eaf7185b
Brandon Gorson <42119471+Bgorson@users.noreply.github.com>
2019-01-28 12:01:47 -0600

    Add files via upload
    Added functionality for multiple maps

--- assets/javascript/mainhub.js
+++ assets/javascript/mainhub.js
@@ -1,17 +1,63 @@
 //NEED DATA TO BE DISPLAYED BETTER
 
 //create Map with location
+
+
 var eLat
 var eLng
+// function initMap() {
+//   // The location of 
+//   var location = {lat: eLat, lng: eLng };
+//   // The map, centered
+//   var map = new google.maps.Map(
+//       document.getElementById('map'), {zoom: 12, center: location});
+//   // The marker, positioned at
+//   var marker = new google.maps.Marker({position: location, map: map});
+// };
+
+// var map;
 function initMap() {
-  // The location of 
-  var location = {lat: eLat, lng: eLng };
-  // The map, centered
-  var map = new google.maps.Map(
-      document.getElementById('map'), {zoom: 12, center: location});
-  // The marker, positioned at
-  var marker = new google.maps.Marker({position: location, map: map});
-};
+  // Get all map canvas with ".maps" and store them to a variable.
+  var maps = document.getElementsByClassName("maps");
+
+  var ids, gps, mapId = '';
+
+  // Loop: Explore all elements with ".maps" and create a new Google Map object for them
+  for (var i = 0; i < maps.length; i++) {
+
+    // Get ID of single div
+    mapId = document.getElementById(maps[i].id);
+    console.log(mapId)
+
+    // Get LatLng stored in data attribute. 
+    // !!! Make sure there is no space in data-attribute !!!
+    // !!! and the values are separated with comma !!!
+    gps = mapId.getAttribute('data-gps');
+    console.log(gps)
+
+    // Convert LatLng to an array
+    gps = gps.split(",");
+
+    // Create new Google Map object for single canvas 
+    map = new google.maps.Map(mapId, {
+      zoom: 15,
+      // Use our LatLng array bellow
+      center: new google.maps.LatLng(parseFloat(gps[0]), parseFloat(gps[1])),
+      mapTypeId: 'roadmap',
+      mapTypeControl: true,
+      zoomControlOptions: {
+        position: google.maps.ControlPosition.RIGHT_TOP
+      }
+    });
+
+    // Create new Google Marker object for new map
+    var marker = new google.maps.Marker({
+      // Use our LatLng array bellow
+      position: new google.maps.LatLng(parseFloat(gps[0]), parseFloat(gps[1])),
+      map: map
+    });
+  }
+}
 
 
 // firebase Info
@@ -21,9 +67,9 @@ var endDate
 var database = firebase.database();
 var name = (localStorage.getItem("groupName"));
 console.log(name)
-var groupRef = (database.ref('group/'+ name));
+var groupRef = (database.ref('group/' + name));
 //NEED TO CHANGE WHEN POPULATE TABLE IS CALLED
-groupRef.on("value", function(snapshot){
+groupRef.once("value", function (snapshot) {
   city = (snapshot.val().city);
   startDate = (snapshot.val().sDate) + "T00%3A00%3A00Z";
   endDate = (snapshot.val().eDate) + "T00%3A00%3A00Z";
@@ -36,82 +82,77 @@ groupRef.on("value", function(snapshot){
 
 //Put in response.Lat.long variable and line them uo  
 function populateTable() {
-  // var location ='denver'
 
-  
-  // console.log(city);
- 
-  var QueryURL= 'https://www.eventbriteapi.com/v3/events/search/?q=coding&location.address='+city+'&start_date.range_start='+startDate+'&start_date.range_end='+endDate+'&token=3RS5KP3QRP5LW3OTLAWF'
-  
+
+  var QueryURL = 'https://www.eventbriteapi.com/v3/events/search/?q=coding&location.address=' + city + '&start_date.range_start=' + startDate + '&start_date.range_end=' + endDate + '&token=3RS5KP3QRP5LW3OTLAWF'
+
   console.log(QueryURL)
 
   $.ajax({
-      url: QueryURL,
-      method: "GET"
-    }).then(function(response) {
-        console.log(response)
-      // Create a new table row element
-      for (i=0;i<2;i++){
-          
-      var tRow = $("<tr Id= '"+ response.events[i].id + "'>");
-  
+    url: QueryURL,
+    method: "GET"
+  }).then(function (response) {
+    console.log(response)
+    // Create a new table row element
+    for (i = 0; i < 5; i++) {
+
+      var tRow = $("<tr Id= '" + response.events[i].id + "'>");
+
       // Methods run on jQuery selectors return the selector they we run on
       // This is why we can create and save a reference to a td in the same statement we update its text
       var Tab1 = $("<td>").text(response.events[i].name.text);
       var Tab2 = $("<td>").text(response.events[i].description.text);
       var Tab3 = $("<td>").text(response.events[i].start.local);
-      var popular= $("<button class = pop eventId=" + response.events[i].id+"' >").text("Interested?")
-   
-      
+      var popular = $("<button class = pop eventId=" + response.events[i].id + "' >").text("Interested?")
+
+
       var mapButton = $("<button class='mapButton' venue=" + response.events[i].venue_id + ">").text("Map");
       // Append the newly created table data to the table row
-      tRow.append(Tab3,Tab1,popular);
-      
-     
+      tRow.append(Tab3, Tab1, popular);
+
+
       // tRow.append(mapDiv);
       // Append the table row to the table body
       // initMap();
       $("tbody").append(tRow);
       $("tbody").append(mapButton);
-      
-      
-      }
 
-    });
-  };
-  //Displaying the map
-  //NEED TO ONLY APPEND MAP TO CORRECT BUTTONS
-  $(document).on('click','.mapButton', function(){
-    var mapButtonDiv= this;
-    var venueid = $(this).attr("venue");
-    var QueryURL= 'https://www.eventbriteapi.com/v3/venues/' + venueid + '/?token=TPQYCAU53IO2TT2FQOOY';
-    $.ajax({
-      url: QueryURL,
-      method: "GET"
-    }).then(function(response) {
-      var mapDiv = $("<div id='map'>");
-      $(mapButtonDiv).replaceWith(mapDiv);
-        console.log(mapButtonDiv)
+
+    }
+
+  });
+};
+//Displaying the map
+//NEED TO ONLY APPEND MAP TO CORRECT BUTTONS
+$(document).on('click', '.mapButton', function () {
+  var mapButtonDiv = this;
+  var venueid = $(this).attr("venue");
+  console.log(mapButtonDiv)
+  var QueryURL = 'https://www.eventbriteapi.com/v3/venues/' + venueid + '/?token=TPQYCAU53IO2TT2FQOOY';
+  var mapDiv = $("<div id='maps" + venueid + "'class='maps'>")
+  $.ajax({
+    url: QueryURL,
+    method: "GET"
+  }).then(function (response) {
     eLat = parseFloat(response.latitude);
     eLng = parseFloat(response.longitude);
-    console.log(eLat);
-    console.log(eLng);
-    initMap();
+    mapDiv.attr("data-gps", eLat + ',' + eLng)
+    $(mapButtonDiv).replaceWith(mapDiv);
+    initMap()
   });
+
 });
-  
-  
 
 
 
 
-$(document).on("click",".pop", function (){
-  var eventId= $(this).attr("eventId")
+$(document).on("click", ".pop", function () {
+  var eventId = $(this).attr("eventId")
   console.log(eventId)
-  var popularity=0
+  var popularity = 0
 
-  var eventRef = (database.ref('group/'+ name + '/'+ eventId+'/'));
-  eventRef.on('value',function(snapshot){
+  var eventRef = (database.ref('group/' + name + '/' + eventId + '/'));
+  eventRef.on('value', function (snapshot) {
     console.log(snapshot.val())
     popularity++
   })
@@ -119,7 +160,7 @@ $(document).on("click",".pop", function (){
     counter: popularity
 
   })
-  
+
 })
 
 
@@ -127,5 +168,5 @@ $(document).on("click",".pop", function (){
 
 
 
-  //When event's popularity is clicked- add eventID and increment
-  //popularity in firebase
+//When event's popularity is clicked- add eventID and increment
+//popularity in firebase
