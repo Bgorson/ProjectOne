d58dab43735a69a92e3de28bf97f774b14fbff47
BGorson <BGorson32@gmail.com>
2019-01-27 20:28:29 -0600

    some changes to mainhub added maps and popularity

--- assets/javascript/mainhub.js
+++ assets/javascript/mainhub.js
@@ -1,6 +1,4 @@
-//Google Map interactions
-
-//Get Lat/Long from AJAX Call
+//NEED DATA TO BE DISPLAYED BETTER
 
 //create Map with location
 var eLat
@@ -24,6 +22,7 @@ var database = firebase.database();
 var name = (localStorage.getItem("groupName"));
 console.log(name)
 var groupRef = (database.ref('group/'+ name));
+//NEED TO CHANGE WHEN POPULATE TABLE IS CALLED
 groupRef.on("value", function(snapshot){
   city = (snapshot.val().city);
   startDate = (snapshot.val().sDate) + "T00%3A00%3A00Z";
@@ -52,20 +51,21 @@ function populateTable() {
     }).then(function(response) {
         console.log(response)
       // Create a new table row element
-      for (i=0;i<1;i++){
+      for (i=0;i<2;i++){
           
-      var tRow = $("<tr>");
+      var tRow = $("<tr Id= '"+ response.events[i].id + "'>");
   
       // Methods run on jQuery selectors return the selector they we run on
       // This is why we can create and save a reference to a td in the same statement we update its text
       var Tab1 = $("<td>").text(response.events[i].name.text);
       var Tab2 = $("<td>").text(response.events[i].description.text);
       var Tab3 = $("<td>").text(response.events[i].start.local);
-      var Tab4 = $("<td>").text(response.events[i].id);
-      var mapDiv = $("<div id='map'>");
-      var mapButton = $("<button id='mapButton' venue=" + response.events[i].venue_id + ">").text("Map");
+      var popular= $("<button class = pop eventId=" + response.events[i].id+"' >").text("Interested?")
+   
+      
+      var mapButton = $("<button class='mapButton' venue=" + response.events[i].venue_id + ">").text("Map");
       // Append the newly created table data to the table row
-      tRow.append(Tab1,Tab2,Tab3,Tab4);
+      tRow.append(Tab3,Tab1,popular);
       
      
       // tRow.append(mapDiv);
@@ -73,19 +73,25 @@ function populateTable() {
       // initMap();
       $("tbody").append(tRow);
       $("tbody").append(mapButton);
-      $("tbody").append(mapDiv);
+      
+      
       }
 
     });
   };
-  $(document).on('click','#mapButton', function(){
+  //Displaying the map
+  //NEED TO ONLY APPEND MAP TO CORRECT BUTTONS
+  $(document).on('click','.mapButton', function(){
+    var mapButtonDiv= this;
     var venueid = $(this).attr("venue");
     var QueryURL= 'https://www.eventbriteapi.com/v3/venues/' + venueid + '/?token=TPQYCAU53IO2TT2FQOOY';
     $.ajax({
       url: QueryURL,
       method: "GET"
     }).then(function(response) {
-        console.log(response)
+      var mapDiv = $("<div id='map'>");
+      $(mapButtonDiv).replaceWith(mapDiv);
+        console.log(mapButtonDiv)
     eLat = parseFloat(response.latitude);
     eLng = parseFloat(response.longitude);
     console.log(eLat);
@@ -98,28 +104,28 @@ function populateTable() {
 
 
 
-// function setGroupInfo(groupName,location,startDate,endDate) {
-// database.ref('group/').push({
-// name: groupName,
-// loc:location,
-// sDate:startDate,
-// eDate:endDate,
-// events: {}
-// });
-// } 
 
+$(document).on("click",".pop", function (){
+  var eventId= $(this).attr("eventId")
+  console.log(eventId)
+  var popularity=0
+
+  var eventRef = (database.ref('group/'+ name + '/'+ eventId+'/'));
+  eventRef.on('value',function(snapshot){
+    console.log(snapshot.val())
+    popularity++
+  })
+  eventRef.set({
+    counter: popularity
+
+  })
+  
+})
 
 
 // var eventIdUrl= 'https://www.eventbriteapi.com/v3/events/'+ eventId + '/'
 
-// console.log(eventIdUrl)
-// $.ajax({
-//     url: eventIdUrl,
-//     method: "GET"
-//   }).then(function(response) {
-//     console.log(response)
-//   })
 
 
-//   //When event's popularity is clicked- add eventID and increment
-//   //popularity in firebase
+  //When event's popularity is clicked- add eventID and increment
+  //popularity in firebase
