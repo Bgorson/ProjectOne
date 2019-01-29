74132bc7f9bd4693d2f6881dce0da040b5075efb
Ryan Gartner <ryanjgartner@gmail.com>
2019-01-28 21:31:18 -0600

    Calendar additions

--- assets/javascript/mainhub.js
+++ assets/javascript/mainhub.js
@@ -107,7 +107,7 @@ function populateTable() {
      
 
       var mapButton = $("<button class='mapButton' venue=" + response.events[i].venue_id + ">").text("Map");
-      var calendarButton = $("<button class = calendarButton " + response.events[i].id + "' >").text("Add to Calendar");
+      var calendarButton = $("<button class = 'calendarButton'" + response.events[i].start + "' >").text("Add to Calendar");
       // Append the newly created table data to the table row
       tRow.append(Tab3, Tab1, popular);
 
@@ -144,10 +144,11 @@ $(document).on('click', '.mapButton', function () {
 });
 
 //Adding the Calendar
-document.getElementsByClassName('calendarButton')[0].appendChild(createCalendar({data:{title:"this is the title of my event", start: new Date(), duration: 90}}));
-
-
-
+$(document).on("click", ".calendarButton", function() {
+  document.getElementsByClassName('calendarButton')[0].appendChild(createCalendar({data:{title:"this is the title of my event", start: new Date(), duration: 90}}));
+  $(".calendarButton").attr("disabled", true);
+});
+  
 
 $(document).on("click", ".pop", function () {
   var eventId = $(this).attr("eventId")
@@ -165,6 +166,7 @@ $(document).on("click", ".pop", function () {
   })
 
 })
+
 
 
 // var eventIdUrl= 'https://www.eventbriteapi.com/v3/events/'+ eventId + '/'
